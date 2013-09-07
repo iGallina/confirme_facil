c388d6a83a754869ae321dedcfc96db1ccf50172
iGallina <ian.gallina@gmail.com>
2013-09-07 17:54:24 -0300

    Commit inicial
    aplicação gerada pelo rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConfirmeFacil::Application.config.secret_key_base = '7cf93d123dc26cbb3d62a694bc6d5f816af3744c1aaeddd3b89543f10a03e33e04e3659fafa4a6cc6568ca84071a7d5581cf6b9699e592402c359692fde55159'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ConfirmeFacil::Application.config.session_store :cookie_store, key: '_confirme_facil_session'
